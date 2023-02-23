42f7dc1e66bf8042b20c54903ebe5bad18ac01a9
oecorrechag <oecorrechag@unal.edu.co>
2023-02-23 18:31:12 -0500

    los graficos

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+
+def scatter_tsne(dataframe, val_x, val_y, val_title):
+    fig = px.scatter(dataframe, x=val_x, y=val_y,
+                     title = val_title,
+                     labels = {val_x: '', val_y: ''},
+                    )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+    return fig
+
+def scatter_tsne_color(dataframe, val_x, val_y, val_color, val_title):
+    fig = px.scatter(dataframe, x=val_x, y=val_y, color=val_color,
+                     title = val_title,
+                     labels = {val_x: '', val_y: '', val_color:''},
+                    )
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+    return fig
