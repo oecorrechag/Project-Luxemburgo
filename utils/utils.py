e7c2cc23643a96bfe7f44d6e416915a7c6001f59
oecorrechag <oecorrechag@unal.edu.co>
2023-02-23 22:14:51 -0500

    prueba de imagenes

--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,9 @@ def scatter_tsne(dataframe, val_x, val_y, val_title):
                      labels = {val_x: '', val_y: ''},
                     )
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+    fig.update_layout(showlegend=False)
+    fig.update_xaxes(visible=False)
+    fig.update_yaxes(visible=False)
     return fig
 
 def scatter_tsne_color(dataframe, val_x, val_y, val_color, val_title):
@@ -14,4 +17,7 @@ def scatter_tsne_color(dataframe, val_x, val_y, val_color, val_title):
                      labels = {val_x: '', val_y: '', val_color:''},
                     )
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+    fig.update_layout(showlegend=False)
+    fig.update_xaxes(visible=False)
+    fig.update_yaxes(visible=False)
     return fig
